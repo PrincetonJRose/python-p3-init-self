f91789e1e0cadc26c491209082ce842303411703
Princeton J. Rose <princetonjrose@gmail.com>
2022-12-29 14:11:41 -0500

    finished lab

--- lib/dog.py
+++ lib/dog.py
@@ -2,3 +2,13 @@
 
 class Dog:
     pass
+    def __init__( self, name, breed="Mutt" ):
+        print( "%s is born!" % name )
+        self.name = name
+        self.breed = breed
+
+    def bark( self ):
+        print( "Woof!" )
+
+    def get_adopted( self, owner_name ):
+        self.owner = owner_name
